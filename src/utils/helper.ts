40a5cdccde174b66d3ce09bc104d9dd8bc2996af
xLsDg <xlsdg@qq.com>
2019-02-01 08:40:10 +0800

    chore(mod): upgrade packages

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,7 @@
-export function px2Rem(px) {
+export function px2Rem(px: number): number {
   return px / 100;
 }
 
-export function px2RemStr(px) {
+export function px2RemStr(px: number): string {
   return `${px2Rem(px)}rem`;
 }
